46eebe9d429670d20eb7cee8e51fb64ac1274de3
Rabin <poudelrabin2004@gmail.com>
2024-07-01 23:59:28 +0545

    v2

---
+++ labsheet/labsheet-2/overloading-fn.cpp
@@ -0,0 +1,46 @@
+// Write a program using the function overloading that converts feet to inches.
+// Use function with no argument,
+// one argument and two arguments.
+//  Decide yourself the types of arguments.
+//  Use pass by reference in any one of the function above.
+
+#include <iostream>
+using namespace std;
+void noarg();
+void witharg(int);
+void witharg(int, int &inch);
+void passbyreference(int &feet);
+
+int main()
+{
+    int feet, inch;
+    cout << "Enter the length in feet: " << endl;
+    cin >> feet;
+    inch = feet * 12;
+    noarg();
+    // witharg(feet);
+    witharg(feet, inch);
+    passbyreference(feet);
+    return 0;
+}
+void noarg()
+{
+    cout << " For the function no argument : \nEnter the length in feet" << endl;
+    int feet;
+    cin >> feet;
+    cout << " No argument\nThe length you entered " << feet << "feet in Inch equals : " << feet * 12 << endl;
+}
+void witharg(int feet)
+{
+
+    cout << "Single argument \nThe length you entered " << feet << "feet in Inch equals : " << feet * 12 << endl;
+}
+void witharg(int feet, int &inch)
+{
+
+    cout << "Double argument \nThe length you entered " << feet << "feet in Inch equals : " << inch << endl;
+}
+void passbyreference(int &feet)
+{
+    cout << "Pass by reference  \nThe length you entered " << feet << "feet in Inch equals : " << feet * 12 << endl;
+}
