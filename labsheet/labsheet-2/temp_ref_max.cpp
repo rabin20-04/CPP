605b3328d25a50b0873e1c25c239b50afa65c4bc
Rabin <poudelrabin2004@gmail.com>
2024-07-02 23:30:59 +0545

    tem-ref

--- labsheet/labsheet-2/temp_ref_max.cpp
+++ labsheet/labsheet-2/temp_ref_max.cpp
@@ -1,10 +1,45 @@
+// Write a function that passes two temperatures by reference and
+//  sets the larger of the two numbers to a value entered by user by using return by reference.
+// prompt user two  temp values
+//  know the largest one then ask a new temp
+// so in the place or address of largest number place the new number  and display the two variable
 #include <iostream>
 using namespace std;
 
+int &tempbyref(int &temp1, int &temp2);
+
 int main()
 {
+    int temp1, update, temp2;
 
-    cout << "Enter " << endl;
+    cout << "Enter two temperature :  " << endl;
+    cout << "First: ";
+    cin >> temp1;
+    cout << "Second: ";
+    cin >> temp2;
+
+    int &largest = tempbyref(temp1, temp2);
+    cout << "largest : " << largest << endl;
+
+    cout << "Enter  temperature to update" << endl;
+    cin >> update;
+
+    largest = update;
+    cout << "largest : " << largest << endl;
+
+    cout << "temp1 : " << temp1 << endl;
+    cout << "temp2 : " << temp2 << endl;
 
     return 0;
+}
+
+int &tempbyref(int &temp1, int &temp2)
+{
+    if (temp1 > temp2)
+    {
+
+        return temp1;
+    }
+    else
+        return temp2;
 }
