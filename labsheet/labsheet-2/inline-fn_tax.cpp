7dead02a57630eae86960a2be3e39a9b3ddc0c7f
Rabin <poudelrabin2004@gmail.com>
2024-07-03 22:08:40 +0545

    fn-inline-v-final

--- labsheet/labsheet-2/inline-fn_tax.cpp
+++ labsheet/labsheet-2/inline-fn_tax.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 class Office
 {
+
 public:
     float tax, rem;
     string name;
     float Emp_salary, salary;
     int position;
+
     void send_calculatedsalary(float);
+
     void clearScreen()
     {
         system("clear");
     }
+
     void getdata()
 
     {
@@ -27,17 +31,18 @@ public:
         {
             cout << "Invalid choice: \nPlease Try again! ";
         }
+
         else if (position == 1)
         {
             clearScreen();
-
+            cout << "Hello " << name << endl;
             cout << "Enter you salary : ";
             cin >> Emp_salary;
 
             send_calculatedsalary(Emp_salary);
 
             cout << fixed << setprecision(2);
-            cout << "Total salary you will receive after applying 10\% tax(" << tax << ") will be :$ " << rem << endl;
+            cout << "\nTotal salary you will receive after applying 10\% tax(" << tax << ") will be : $" << rem << endl;
         }
         else
         {
@@ -51,6 +56,7 @@ inline void Office::send_calculatedsalary(float Emp_salary)
     tax = 0.1 * Emp_salary;
     rem = Emp_salary - tax;
 }
+
 int main()
 {
     Office o1;
