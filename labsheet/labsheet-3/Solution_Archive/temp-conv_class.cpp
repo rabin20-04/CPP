d45bad5e04ec7d8cb2c7beb7d2365d519fa91d8d
Rabin <poudelrabin2004@gmail.com>
2024-07-05 00:04:52 +0545

    final

--- labsheet/labsheet-3/Solution_Archive/temp-conv_class.cpp
+++ labsheet/labsheet-3/Solution_Archive/temp-conv_class.cpp
@@ -1,9 +1,9 @@
 
-// Write a simple program that converts the temperature in Celsius scale to Farenheit scale
+// Write a simple program that converts the temperature in Celsius scale to Fahrenheit scale
 //  and vice versa using the basic concept of class and object.
-//   Make separate classes for Celsius and Farenheit which will have the private members
+//   Make separate classes for Celsius and Fahrenheit which will have the private members
 //    that hold the temperature value and make conversion functions in each class for conversion from one to another.
-//     For example, you need to have a function toFarenheit() in class Celsius that converts to Farenheit scale and returns the value.
+//     For example, you need to have a function toFahrenheit() in class Celsius that converts to Fahrenheit scale and returns the value.
 
 #include <iostream>
 using namespace std;
@@ -16,13 +16,13 @@ private:
     float converted;
 
 public:
-    float toFarenheit(float temp)
+    float toFahrenheit(float temp)
     {
         // return converted = (temp * 1.8) + 32;
         return (temp * 1.8) + 32;
     }
 };
-class Farenheit
+class Fahrenheit
 {
 private:
     float temp;
@@ -39,29 +39,29 @@ public:
 int main()
 {
     Celsius c1;
-    Farenheit f1;
+    Fahrenheit f1;
 
-    cout << "Which temperature do you want to convert ? \n 1)Farenheit to Celsius \n2) Celsius to Farenheit  " << endl;
+    cout << "Which temperature do you want to convert ? \n 1)Fahrenheit to Celsius \n2) Celsius to Fahrenheit  " << endl;
     float choice, value;
     cin >> choice;
 
     if (choice == 1)
     {
         clearScreen();
-        cout << "Enter the Farenheit value : " << endl;
+        cout << "Enter the Fahrenheit value : " << endl;
         cin >> value;
-        cout << "The Celsius value of " << value << " degree Farenheit is : " << c1.toFarenheit(value) << " degree Celsius" << endl;
+        cout << "The Celsius value of " << value << " degree Fahrenheit is : " << c1.toFahrenheit(value) << " degree Celsius" << endl;
 
         // //to view if the result is correct
-        // float apple = c1.toFarenheit(value);
-        // cout << "The Farenheit value of " << apple << " degree Celsius is : " << f1.toCelsius(apple) << " degree Farenheit" << endl;
+        // float apple = c1.toFahrenheit(value);
+        // cout << "The Fahrenheit value of " << apple << " degree Celsius is : " << f1.toCelsius(apple) << " degree Fahrenheit" << endl;
     }
     else if (choice == 2)
     {
         clearScreen();
         cout << "Enter the Celsius value : " << endl;
         cin >> value;
-        cout << "The Farenheit value of " << value << " degree Celsius is : " << f1.toCelsius(value) << " degree Farenheit" << endl;
+        cout << "The Fahrenheit value of " << value << " degree Celsius is : " << f1.toCelsius(value) << " degree Fahrenheit" << endl;
     }
     else
     {
