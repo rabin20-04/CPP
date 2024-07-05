ef45f3eb5075d1f185e7f59444c954c1e94fc0ee
Rabin <poudelrabin2004@gmail.com>
2024-07-05 23:58:22 +0545

    prime-v2

--- labsheet/labsheet-3/Solution_Archive/prime-oop.cpp
+++ labsheet/labsheet-3/Solution_Archive/prime-oop.cpp
@@ -12,8 +12,13 @@ private:
     int num;
 
 public:
-    void show()
+    void show(int num)
     {
+        for (int i=2;i<=num/2;i++)
+        {
+            
+
+        }
     }
 };
 
@@ -26,7 +31,7 @@ int main()
     {
         cout << "Enter any number to check whether it is Prime or not : " << endl;
         cin >> num;
-
+p1.show(num);
         cout << "Do you want to enter more ?\n Enter 'y' or 'n' : ";
         cin >> choice;
     } while (choice == 'y');
