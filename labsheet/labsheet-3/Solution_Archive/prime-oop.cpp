c96f7fb226311080618e45a77897ff93f8949e13
Rabin <poudelrabin2004@gmail.com>
2024-07-06 00:33:00 +0545

    prime final

--- labsheet/labsheet-3/Solution_Archive/prime-oop.cpp
+++ labsheet/labsheet-3/Solution_Archive/prime-oop.cpp
@@ -4,37 +4,56 @@
 // The response can be 'y' or 'n'. Don't forget to use the object-oriented concept.
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Prime
 {
 private:
-int check;
-int count;
     int num;
 
 public:
-    void show(int num)
-    {
-        for (int i=2;i<=num/2;i++)
-        {
-          
-            
+    Prime(int n) : num(n){};
+    void show()
 
+    {
+        if (num < 1)
+        {
+            cout << "Invalid !\nEnter a positive number" << endl;
+            return;
+        }
+        bool isPrime = true;
+        for (int i = 2; i <= sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+        {
+            cout << num << " is a prime number." << endl;
+        }
+        else
+        {
+            cout << num << " not a Prime number! " << endl;
         }
     }
 };
 
 int main()
 {
-    Prime p1;
     int num;
     char choice;
+
     do
     {
-        cout << "Enter any number to check whether it is Prime or not : " << endl;
+        cout << "Check whether a number is prime or not !\n Enter any number : ";
         cin >> num;
-p1.show(num);
+
+        Prime p1(num);
+        p1.show();
         cout << "Do you want to enter more ?\n Enter 'y' or 'n' : ";
         cin >> choice;
     } while (choice == 'y');
