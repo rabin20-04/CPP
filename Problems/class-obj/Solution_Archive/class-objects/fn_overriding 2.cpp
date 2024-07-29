d68ff3a0e1e222d9efdafe2714e0a0630f09bb76
Rabin <poudelrabin2004@gmail.com>
2024-07-29 18:08:42 +0545

    fn

---
+++ Problems/class-obj/Solution_Archive/class-objects/fn_overriding 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() {
+        cout << "Drawing a shape" << endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing a circle" << endl;
+    }
+};
+
+class Rectangle : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing a rectangle" << endl;
+    }
+};
+
+int main() {
+    Shape* shape1 = new Circle();
+    Shape* shape2 = new Rectangle();
+
+    shape1->draw(); // Output: Drawing a circle
+    shape2->draw(); // Output: Drawing a rectangle
+
+    delete shape1;
+    delete shape2;
+
+    return 0;
+}
