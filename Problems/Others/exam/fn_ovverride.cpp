4eebcc13930dee238e26acae51b7258e8ac57c8a
Rabin <poudelrabin2004@gmail.com>
2024-08-11 22:53:16 +0545

    exam

--- Problems/Others/exam/fn_ovverride.cpp
+++ Problems/Others/exam/fn_ovverride.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 using namespace std;
 
+class base
+{
+public:
+    virtual void show()
+    {
+        cout << "Hello from base class" << endl;
+    }
+};
+class drived : public base
+{
+public:
+    void show() override
+    {
+        cout << "Hello from derived class " << endl;
+    }
+};
+
 int main()
 {
+    base *b;
+    drived d;
+    b = &d;
+    b->show(); // in place of b.show();
 
-    cout << "Enter " << endl;
+    // key point for runtime there are others also but easy is use
+    // base as pointer
+    // take address of derived in the pointer
+    // call function using ->
+    // dont forget to use virtual ,,, override ,,,
 
     return 0;
 }
