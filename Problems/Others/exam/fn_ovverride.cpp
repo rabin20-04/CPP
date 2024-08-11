64a328b910a9b5022619a94374e7b092827275ca
Rabin <poudelrabin2004@gmail.com>
2024-08-11 22:57:23 +0545

    exam

--- Problems/Others/exam/fn_ovverride.cpp
+++ Problems/Others/exam/fn_ovverride.cpp
@@ -23,13 +23,14 @@ int main()
     base *b;
     drived d;
     b = &d;
-    b->show(); // in place of b.show();
+    b->show(); // in place of b.show();    and ---// Calls derived class show() due to runtime polymorphism
 
+    // ------------------------------------------if used simply b.show and d.show it is static polymorphism
     // key point for runtime there are others also but easy is use
     // base as pointer
     // take address of derived in the pointer
     // call function using ->
-    // dont forget to use virtual ,,, override ,,,
+    // dont forget to use virtual ,,, override ,,,here override word is optional
 
     return 0;
 }
