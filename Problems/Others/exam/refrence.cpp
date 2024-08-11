70f8cf281f8b694449f6237b67b0c2ab85f8ff7b
Rabin <poudelrabin2004@gmail.com>
2024-08-11 22:17:57 +0545

    added

--- Problems/Others/exam/refrence.cpp
+++ Problems/Others/exam/refrence.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   
-    cout << "Enter " << endl;
-   
+void avgref(int &, int &);
+
+int main()
+{
+    int aa = 2, bb = 7;
+    avgref(aa, bb);
+    cout << aa << endl
+         << bb << endl;
     return 0;
-   
+}
+
+void avgref(int &a, int &b)
+{
+    int temp = (a + b) / 2;
+    if (a > b)
+    {
+        a = temp;
+    }
+    else
+        b = temp;
 }
